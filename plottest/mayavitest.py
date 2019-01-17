de071abc8e01b57e23d2b484a650210c9764f85e
Daniel Lawson <daniellawson9999@gmail.com>
2019-01-16 18:26:27 -0600

    more recent

---
+++ plottest/mayavitest.py
@@ -0,0 +1,81 @@
+import numpy as np
+from mayavi import mlab
+
+def test_surf():
+    """Test surf on regularly spaced co-ordinates like MayaVi."""
+    def f(x, y):
+        sin, cos = np.sin, np.cos
+        return sin(x + y) + sin(2 * x - y) + cos(3 * x + 4 * y)
+
+    x, y = np.mgrid[-7.:7.05:0.1, -5.:5.05:0.05]
+    s = mlab.surf(x, y, f)
+    #cs = contour_surf(x, y, f, contour_z=0)
+    return s
+
+class Sphere():
+    def __init__(self,x = 0, y = 0, z = 0, d = 10,c = (0,0,0), above_surface = True):
+        d = d / 2
+        if above_surface:
+            shift = d
+        else:
+            shift = 0
+        self.u = np.linspace(0, 2 * np.pi, 100)
+        self.v = np.linspace(0, np.pi, 100)
+        self.x = d * np.outer(np.cos(self.u), np.sin(self.v)) + x
+        self.y = d * np.outer(np.sin(self.u), np.sin(self.v)) + y
+        self.z = d * np.outer(np.ones(np.size(self.u)), np.cos(self.v)) + z + shift
+        self.c = c
+    def plot(self):
+        mlab.mesh(self.x,self.y,self.z,color = self.c)
+        
+class RectangularPrism():
+    def __init__(self,x = 0, y = 0, z= 0, w = 4, l = 4, h = 4,c = (0,0,0), above_surface = True):
+        self.w = w /2
+        self.l = l / 2
+        self.h = h / 2     
+        if above_surface:
+            shift = self.h
+        else:
+            shift = 0
+        r = [-1,1]
+        self.X, self.Y = np.meshgrid(r, r)
+        self.x = x
+        self.z = z + shift
+        self.y = y
+        
+        self.ones = np.ones((2,2))
+        theta = np.pi / 3
+        x = self.X
+        y = self.Y
+        #self.X = x * np.round(np.cos(theta),4) - y * np.round(np.sin(theta),4)
+        #self.Y = y * np.round(np.cos(theta),4) - x * np.round(np.sin(theta),4)
+        self.c = c
+    def plot(self, c = (0,0,0)):
+        def mesh(x,y,z,color):
+            theta = np.pi /2
+            theta = 0
+            theta = np.pi
+            
+           # x = X * np.round(np.cos(theta),4) - Y * np.round(np.sin(theta),4)
+            #y = Y * np.round(np.cos(theta),4) - X * np.round(np.sin(theta),4)
+            mlab.mesh(x,y,z,color = color)
+        mesh(self.X * self.w + self.x, self.Y * self.l + self.y, self.ones * self.h + self.z , color = self.c)
+        mesh(self.X * self.w + self.x, self.Y * self.l + self.y,self.ones * -self.h + self.z,color = self.c)
+        mesh(self.X * self.w + self.x, -self.l * self.ones + self.y,self.Y * self.h + self.z,color = self.c)
+        mesh(self.X * self.w + self.x, self.ones*self.l + self.y,self.Y * self.h + self.z,color = self.c)
+        mesh(self.ones * self.w + self.x,self.X * self.l + self.y,self.Y * self.h + self.z,color = self.c)
+        mesh(-self.w * self.ones + self.x ,self.X * self.l + self.y,self.Y*self.h + self.z,color = self.c)
+mlab.figure(size=(400, 300), bgcolor = (1,1,1))
+
+#floor_3_3 = RectangularPrism(0,0,-1,w=23.5*3,l=23.5*3,h=2,c=(.39,.39,.39), above_surface = False)
+d = 14.5 / np.sqrt(2)
+m1 = RectangularPrism(-d,d,0,w=2,l=2,h=2,c = (1,1,0))
+#m2 = Sphere(0,0,0,d = 2.75,c = (1,1,1))
+m3 = Sphere(d,-d,0,d = 2.75,c = (1,1,1))
+
+#floor_3_3.plot()
+m1.plot()
+#m2.plot()
+m3.plot()
+
+mlab.show()
---
+++ plottest/other.py
@@ -0,0 +1,58 @@
+from mayavi import mlab
+import numpy as np
+from tvtk.tools import visual
+class Environment():        
+    def __init__(self):
+        f = mlab.figure(size=(900,500),bgcolor = (1,1,1))
+        visual.set_viewer(f)  
+        square_width = 23.5
+        self.square_width = 23.5
+        a_side = 34.5
+        tape_height = .2
+        #mlab.view(focalpoint=[0,0,0],elevation=180,figure=f)
+        #mlab.roll(0)
+        #distance between minerals
+        d = 14.5 / np.sqrt(2)
+        #color for silver
+        silver = (.8,.8,.8)
+        #reate field
+        floor_3_3 = visual.box(x=0,y=0,z=-1, length = 23.5*3,height = 23.5*3,width = 2,color = (.4,.4,.4))  
+        #randomize starting locations
+        locations = [[-d,d],[0,0],[d,-d]]
+        np.random.shuffle(locations)
+        #place minerals
+        gold_mineral = visual.box(x=locations[0][0],y=locations[0][1],z=1, length=2,height=2,width=2, color = (1,1,0))
+        silver_mineral_1 = visual.sphere(x=locations[1][0],y=locations[1][1],z=2.75/2,radius =2.75/2,color = silver)
+        silver_mineral_2 = visual.sphere(x=locations[2][0],y=locations[2][1],z=2.75/2,radius =2.75/2,color = silver)
+        
+        #randomly pick the red or blue side
+        r = np.round(np.random.random(1)[0])
+        b = 1 - r
+        tape_color = (r,0,b)
+        #23.5 is the diameter of a square
+        #place the crater tape
+        vertical_lander_tape = visual.box(x=-square_width*3/2 + 1,y=a_side/2 - square_width*3/2,z=tape_height,length = 2, height = a_side, width = tape_height,color=tape_color)
+        h_lander_tape = visual.box(x=-square_width*3/2 + a_side/2,y=-a_side/2,z=tape_height,length = 2, height = a_side * np.sqrt(2), width = tape_height,color=tape_color)
+        h_lander_tape.rotate(45,axis = [0,0,1],origin = [h_lander_tape.x,h_lander_tape.y,h_lander_tape.z])
+        marker_left = visual.box(x=square_width/2 + 1, y =square_width,z=tape_height,length=2,height = square_width,width=tape_height,color=tape_color)
+        marker_right = visual.box(x=3*square_width/2 -1, y =square_width,z=tape_height,length=2,height = square_width,width=tape_height,color=tape_color)
+        marker_bottom = visual.box(x=square_width,y=square_width/2 + 1, z = tape_height,length=square_width,height=2,width=tape_height,color=tape_color)
+        marker_bottom = visual.box(x=square_width,y=3*square_width/2 - 1, z = tape_height,length=square_width,height=2,width=tape_height,color=tape_color)
+
+        #mlab.view(focalpoint=[d,d,0],distance=64, elevation=-80)
+        self.move_robot(-square_width,-square_width)
+        mlab.draw()
+        mlab.show()
+        print(mlab.view())
+        
+    def move_robot(self,x,y):
+        angle_d = 10
+        angle_r = 10 * np.pi / 180
+        view_distance = self.square_width * np.sqrt(2)
+        shift = view_distance / np.sqrt(2)
+        view_radius = view_distance / np.cos(angle_r)  
+        #maybe add a z
+        fp = [x+shift,y+shift,0]
+        print(fp)
+        mlab.view(focalpoint=fp, distance=view_radius, elevation=-90 + angle_d)   
+env = Environment()
