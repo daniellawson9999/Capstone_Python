8e3746e82e3a314fadf7c5317a3b34d55985ab6a
Daniel Lawson <daniellawson9999@gmail.com>
2019-06-10 22:24:37 -0500

    backup

--- Robot/Rotation/test.py
+++ Robot/Rotation/test.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib
 matplotlib.interactive(True)
 
-training_name = "sqa_test_rgb_limited_actions_2"
+training_name = "sqa_test_rgb_full_actions_2"
 env_file_name = training_name
 model_load_name = training_name
 network_type = Network.S_TO_QA
@@ -15,6 +15,6 @@ training_file_name = training_name
 agent = Agent(env_type = Env.MULTI, env_file_name = env_file_name, 
               network_type = network_type,  training_name = training_name, training_file_name = training_file_name, model_load_name = model_load_name,load_model=True, training_mode = Modes.TESTING)
 
-agent.TEST_MAX_MOVES = 150
-wins, losses, reached_max = agent.test(epochs=50)
+agent.TEST_MAX_MOVES = 200
+wins, losses, reached_max = agent.test(epochs=10)
 
